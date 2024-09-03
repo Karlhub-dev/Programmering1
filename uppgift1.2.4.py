e9bac1d2e340fb2bd1dd5ddb2ec5ea868fe49da9
Karlhub-dev <karwin12@utb.karlskoga.se>
2024-09-03 09:08:11 +0200

    uppgift1.2.4 klar

---
+++ uppgift1.2.4.py
@@ -0,0 +1,24 @@
+tal=float(input("Skriv ett tal "))
+
+test=tal%2
+if test == 1:
+    print("Ditt tal är udda")
+elif test == 0:
+    print("Ditt tal är jämnt")
+else:
+    print("Ditt tal är varken udda eller jämnt, det är inte ett heltal")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
