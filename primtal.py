ddb47993062f378cfc6aa1f4d0db71e6646eb5b3
Karlhub-dev <karwin12@utb.karlskoga.se>
2024-09-03 08:19:11 +0200

    Uppgift1.2.2 klar

---
+++ primtal.py
@@ -0,0 +1,21 @@
+import math
+
+tal = int(input("Skriv ett tal "))
+n=2
+
+
+while tal%n == 0:
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ uppgift1.2.2.py
@@ -0,0 +1,31 @@
+p=float(input("Hur många poäng fick du på provet? "))
+
+if p < 25:
+     print("Du är tyvärr underkänd")
+elif p < 30:
+    print("Ditt betyg är E")
+elif p < 35:
+    print("Ditt betyg är D")
+elif p < 40:
+    print("Ditt betyg är C")
+elif p < 45:
+    print("Ditt betyg är B")
+else:
+    print("Ditt betyg är A")
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
