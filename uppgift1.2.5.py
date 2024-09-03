368e0168366d0d86ac7b9e063a0fae5d955fd6b3
Karlhub-dev <karwin12@utb.karlskoga.se>
2024-09-03 09:17:27 +0200

    uppgift1.2.5 klar

---
+++ uppgift1.2.5.py
@@ -0,0 +1,27 @@
+tal=round(float(input("Skriv ett tal ")))
+
+if tal < 0:
+    print(tal, "är negativt")
+elif tal > 0:
+    print(tal, "är positivt")
+else:
+    print("Talet är noll")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
