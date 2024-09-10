15b1b4083c033352af87505897e17e06b87d18d8
Karlhub-dev <karwin12@utb.karlskoga.se>
2024-09-10 08:47:00 +0200

    uppgift1.3.5 klar

--- testing.py
+++ testing.py
@@ -1,5 +1,13 @@
+ord=str(input("Skriv en meningen eller ett ord "))
+if ord.count(" ")>0:
+    a="Din mening är en palondrom"
+elif ord.count(" ")==0:   
+    a="Ditt ord är en palondrom"
+ord=ord.strip(" ")
 
 
+if ord == ord[::-1]:
+    print(a)
 
 
 
---
+++ uppgift1.3.5.py
@@ -0,0 +1,3 @@
+text=str(input("Vad är din mening? "))
+count=str(input("Vilka tecken vill du leta efter? "))
+print(text, "innehåller", text.count(count), count)
