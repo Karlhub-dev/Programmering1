3b778a21be865e2122504dafc5d56980f7921a5e
Karlhub-dev <karwin12@utb.karlskoga.se>
2025-01-30 08:52:06 +0100

    uppgift1.8.5 klar

---
+++ uppgift1.8.5.py
@@ -0,0 +1,13 @@
+def topercent():
+    decimal=float(input("Vad är decimal talet? "))
+    procent=round(decimal*100,3)
+    return(procent)
+
+print("Talet i procent blir", topercent())
+
+        
+
+
+
+
+    
