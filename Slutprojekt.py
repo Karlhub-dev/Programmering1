bb44c5d44e7e88ff290be2640438d893617eeade
Karlhub-dev <karwin12@utb.karlskoga.se>
2025-04-24 09:23:20 +0200

    slutprojekt inte klar

--- Slutprojekt.py
+++ Slutprojekt.py
@@ -1,7 +1,8 @@
 import random
+import time
 
-color=["Spader", "Hjärter", "Ruter", "Klöver"]
-value=["Ess", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Knekt", "Dam", "Kung"]
+color=["♠️", "♥️", "♦️", "♣️"]
+value=["E", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Kn", "D", "K"]
 kortlekList=[] 
 for loop in range(3): 
     for colorloop in range(4): 
@@ -18,13 +19,13 @@ random.shuffle(kortlekList)
 
 def value(kort1): 
     kort=kort1[1]
-    if kort=="Ess":
+    if kort=="E":
         cvalue=11
-    elif kort=="Knekt":
+    elif kort=="Kn":
         cvalue=10
-    elif kort=="Dam":
+    elif kort=="D":
         cvalue=10
-    elif kort=="Kung":
+    elif kort=="K":
         cvalue=10
     else:
         cvalue=int(kort)
@@ -33,20 +34,46 @@ def value(kort1):
 # Omvandlar klädda kort till en int och vanliga kort till int #
 ###############################################################
 
+def handprint(hand):
+    antalc=len(hand)
+    kortmening=""
+    for cardnum in range(0, antalc):
+        kort1=hand[cardnum]
+        color = str(kort1[0])
+        value = str(kort1[1])
+        kort1= color + "  " + value
+        kortmening+=kort1
+    return(kortmening)
+def cprint(kort1):
+    color = str(kort1[0])
+    value = str(kort1[1])
+    kort1= color + "  " + value
+    return(kort1)
+###########################################
+# Omvandlar korten från lista till sträng #
+###########################################
 
 playerhand=[]
 dealerhand=[]
-for startcards in range(2):
-    playerhand.append(kortlekList.pop(0))
-    
-    dealerhand.append(kortlekList.pop(0))
+playerhand.append(kortlekList.pop(0))
+dealerhand.append(kortlekList.pop(0))
 ################################################################################
-# Tar fyra kort från kortlekarna och appendar till spelarens och dealerns hand #
+# Tar två kort från kortlekarna och appendar till spelarens och dealerns hand #
 ################################################################################
 
 
-print("Du har", playerhand)   
-print("Dealern har", dealerhand[0])
+print("Spelet har börjat...")
+time.sleep(1.5)
+print("Du har", handprint(playerhand))  
+time.sleep(1.5) 
+print("Dealern har", handprint(dealerhand))
+time.sleep(1.5)
+playerhand.append(kortlekList.pop(0))
+dealerhand.append(kortlekList.pop(0))
+print("Du har", handprint(playerhand))
+###################################################################################
+# Berättar vad spelaren och dealern har för startkort samt appendar två kort till #
+###################################################################################
 
 
 def val(): #Felkollar frågan "slå eller stå?"
@@ -57,28 +84,81 @@ def val(): #Felkollar frågan "slå eller stå?"
     return(val)  
   
 playercvalue = (value(playerhand[0])+value(playerhand[1]))
+dealercvalue = (value(dealerhand[0])+value(dealerhand[1]))
 print(playercvalue)
+print(dealercvalue)
+
+
 while True:
+
     if playercvalue == 21:
             print("BLACKJACK!!")
             break
+    
+
     val1=val()
     if val1 == "slå":
+
+        # Ger spelaren ett till kort #
         playerhand.append(kortlekList.pop(0))
-        print("Du har", playerhand)
+        print("Du får...")
+        time.sleep(1.5)
+        print(cprint(playerhand[-1]))
+        time.sleep(1)
+        print("Du har", handprint(playerhand))
         playercvalue += value(playerhand[-1])
         print(playercvalue)
+
+        # Kollar om spelaren fått blackjack eller blivt tjock #
         if playercvalue > 21:
-            print("HAHA! du blev tjock, casinot tar dina pengar")
+            print("HAHA! Du blev tjock, casinot tar dina pengar")
             break
         elif playercvalue == 21:
-            print("BLACKJACK!!")
+            print("Dina korts värde är 21, grattis")
             break
 
 
     elif val1 == "stå":
-        print("Dealern har ", dealerhand)
-        break
+        print("Dealern har...")
+        time.sleep(1.5)
+        print(handprint(dealerhand))
+
+        while True:
+            if dealercvalue > 21:
+                print("Dealern blev tjock! Du vinner!")
+                break
+            elif dealercvalue == 21:
+                print("Dealern har blackjack! Du förlorar")
+                break
+            elif dealercvalue >= 17:
+                break
+            dealerhand.append(kortlekList.pop(0))
+            print("Dealern får...")
+            time.sleep(1.5)
+            print(cprint(dealerhand[-1]))
+            time.sleep(1)
+            print("Dealern har", handprint(dealerhand))
+            dealercvalue += value(dealerhand[-1])
+            print(dealercvalue)
+#####################################################################
+# Ger dealern kort och kollar om den får blackjack eller blir tjock #
+#####################################################################
+
+        time.sleep(1.5)
+        if dealercvalue > 21:
+            break
+        elif dealercvalue > playercvalue:
+            print("Du förlorade! Casinot tar dina pengar")
+            break
+        elif playercvalue > dealercvalue:
+            print("Du vann!")
+            break
+        elif playercvalue == dealercvalue:
+            print("Det blev lika, du får tillbaka dina pengar")
+
+        
+
+    
 #########################################
 # Frågar om spelaren vill slå eller stå #
 #########################################
